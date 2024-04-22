7a01671527c2b5d187f800c43cd4e0fb256b2d26
Charles Zhang <szinubuntu@gmail.com>
2024-04-22 19:01:50 -0400

    Initial library setup and view binding

--- WPFLiveStockPlotting/MainWindow.xaml.cs
+++ WPFLiveStockPlotting/MainWindow.xaml.cs
@@ -1,24 +1,42 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFLiveStockPlotting
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
+
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 4, 9, 16, 25 };
+            WpfPlot1.Plot.Add.Scatter(dataX, dataY);
+            WpfPlot1.Refresh();
         }
+        #endregion
+
+        #region Public View Properties
+        private string? _DataSource;
+        public string? DataSource { get => _DataSource; set => SetField(ref _DataSource, value); }
+        #endregion
+
+        #region Data Binding - DO NOT FORGET TO IMPLEMENT INotifyPropertyChanged ON THE WINDOW CLASS
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        protected bool SetField<TType>(ref TType field, TType value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<TType>.Default.Equals(field, value)) return false;
+            field = value;
+            NotifyPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
     }
 }
